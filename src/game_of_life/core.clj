a34f06d1b66ca60410c29679865d7255d99708c2
Mayur Dave <madave@ebay.com>
2018-08-28 20:18:53 +0100

    Generated Grid with some alive

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -4,3 +4,10 @@
   "Generates a grid of size squared"
   [size]
   (repeat size (repeat size :empty)))
+
+(defn generate-seed
+  "Generates a grid with some cells alive"
+  [size]
+  (let [grid (generate-grid size)]
+    (for [row grid cell row]
+      (if ( = (rand-int 7) 0) :alive :empty))))
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -10,3 +10,7 @@
             [:empty :empty :empty :empty ]
             ]
             (generate-grid 4)))))
+
+(deftest can-randomly-generate-with-life
+  (testing "the grid will have at least one alive cell"
+    (is (map #(contains? % :alive) (generate-seed 4)))))
